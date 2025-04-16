8c656b1303a1cd3f7d388713af3e1221b94696c7
Juan Flores <elgalleto12393@gmail.com>
2025-04-16 17:35:20 -0600

    feat: initialize module imports for application setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { GoalsModule } from './goals/goals.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    UserModule,
+    AuthModule,
+    TransactionsModule,
+    GoalsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
