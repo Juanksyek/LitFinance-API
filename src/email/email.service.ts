14d469c3964e78f3f1c4db120cfafb6a108ef012
Juan Flores <elgalleto12393@gmail.com>
2025-04-16 17:36:18 -0600

    feat: implement EmailService for sending confirmation emails

---
+++ src/email/email.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Resend } from 'resend';
+
+@Injectable()
+export class EmailService {
+    private resend = new Resend(process.env.RESEND_API_KEY);
+
+    async sendConfirmationEmail(to: string, token: string, nombre: string) {
+        const confirmUrl = `${process.env.FRONTEND_URL}${token}`;
+        await this.resend.emails.send({
+            from: 'LitFinance <onboarding@resend.dev>',
+            to,
+            subject: 'Confirma tu cuenta',
+            html: `
+        <h2>Hola ${nombre}</h2>
+        <p>Gracias por registrarte en LitFinance App.</p>
+        <p>Para activar tu cuenta, da clic en el siguiente botón:</p>
+        <a href="${confirmUrl}" style="background-color:#53F29D;padding:10px 15px;border-radius:5px;text-decoration:none;color:#000;">Confirmar cuenta</a>
+        <p>Este enlace expirará en 30 minutos.</p>
+      `,
+        });
+    }
+}
