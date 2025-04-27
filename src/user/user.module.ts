5272ea9bce48e6a02a605b2213fcabd0abeabc66
Juan Flores <elgalleto12393@gmail.com>
2025-04-27 00:57:13 -0600

    feat: add CleanupService to UserModule providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,13 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User, UserSchema } from './schemas/user.schema/user.schema';
+import { CleanupService } from './services/cleanup.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, CleanupService],
   exports: [MongooseModule],
 })
 export class UserModule {}
