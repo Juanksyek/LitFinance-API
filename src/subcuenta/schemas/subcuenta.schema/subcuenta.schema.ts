f391438da176ea954e6b1ff8e6dd73a33e24b3f7
Juan Flores <elgalleto12393@gmail.com>
2025-05-11 16:30:03 -0600

    feat: create Subcuenta schema with properties and validation

---
+++ src/subcuenta/schemas/subcuenta.schema/subcuenta.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type SubcuentaDocument = Subcuenta & Document;
+
+@Schema({ timestamps: true })
+export class Subcuenta {
+  @Prop({ required: true })
+  nombre: string;
+
+  @Prop({ required: true })
+  cantidad: number;
+
+  @Prop({ required: true })
+  moneda: string;
+
+  @Prop()
+  simbolo: string;
+
+  @Prop()
+  color: string;
+
+  @Prop({ default: null })
+  cuentaId?: string;
+
+  @Prop({ default: false })
+  afectaCuenta: boolean;
+
+  @Prop({ required: true })
+  userId: string;
+}
+
+export const SubcuentaSchema = SchemaFactory.createForClass(Subcuenta);
