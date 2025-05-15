7e3e69b3304086fddf3637edbff641b812fdeffc
Juan Flores <elgalleto12393@gmail.com>
2025-05-14 18:30:50 -0600

    feat: integrate Mongoose schemas for transactions, subcuenta, cuenta, and subcuentaHistorial in TransactionsModule

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
+import { Transaction, TransactionSchema } from './schemas/transaction.schema/transaction.schema';
+import { SubcuentaHistorial, SubcuentaHistorialSchema } from '../subcuenta/schemas/subcuenta-historial.schema/subcuenta-historial.schema';
+import { Cuenta, CuentaSchema } from '../cuenta/schemas/cuenta.schema/cuenta.schema';
+import { Subcuenta, SubcuentaSchema } from '../subcuenta/schemas/subcuenta.schema/subcuenta.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Transaction.name, schema: TransactionSchema },
+      { name: SubcuentaHistorial.name, schema: SubcuentaHistorialSchema },
+      { name: Cuenta.name, schema: CuentaSchema },
+      { name: Subcuenta.name, schema: SubcuentaSchema },
+    ]),
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
+  exports: [TransactionsService],
 })
 export class TransactionsModule {}
