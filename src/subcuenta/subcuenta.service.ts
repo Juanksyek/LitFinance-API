b556e2e61cc41ef626832aa1226e3914cb35dd1e
Juan Flores <elgalleto12393@gmail.com>
2025-07-30 18:18:10 -0600

    fix: restore missing comment in subcuenta.service.ts

--- src/subcuenta/subcuenta.service.ts
+++ src/subcuenta/subcuenta.service.ts
@@ -1,3 +1,4 @@
+// commnt
 import { Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
