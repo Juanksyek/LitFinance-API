c63b77ae73326e75a99049477fc5cc75d80c05cd
Juan Flores <elgalleto12393@gmail.com>
2025-09-04 12:08:42 -0600

    fix: remove unnecessary comment about daily cron job in recurrentes-cron.service.ts

--- src/recurrentes/recurrentes-cron/recurrentes-cron.service.ts
+++ src/recurrentes/recurrentes-cron/recurrentes-cron.service.ts
@@ -6,7 +6,6 @@ import { RecurrentesService } from '../recurrentes.service';
 export class RecurrentesCronService {
   constructor(private readonly recurrentesService: RecurrentesService) {}
 
-  // Cron diario a las 1:00 p.m.
   @Cron(CronExpression.EVERY_DAY_AT_1PM)
   async ejecutarRecordatorios() {
     console.log('Ejecutando cron diario de recordatorios...');
