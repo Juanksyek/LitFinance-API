5ec7143a62bc60afe8fc26ea58f2749ef721418d
Juan Flores <elgalleto12393@gmail.com>
2025-11-28 15:54:44 -0600

    fix: add UtilsModule and Subcuenta schema to CuentaModule imports

--- src/cuenta/cuenta.module.ts
+++ src/cuenta/cuenta.module.ts
@@ -4,18 +4,22 @@ import { CuentaService } from './cuenta.service';
 import { CuentaController } from './cuenta.controller';
 import { Cuenta, CuentaSchema } from './schemas/cuenta.schema/cuenta.schema';
 import { User, UserSchema } from '../user/schemas/user.schema/user.schema';
+import { Subcuenta, SubcuentaSchema } from '../subcuenta/schemas/subcuenta.schema/subcuenta.schema';
 import { MonedaModule } from '../moneda/moneda.module';
 import { CuentaHistorialModule } from '../cuenta-historial/cuenta-historial.module';
 import { UserModule } from '../user/user.module';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
   imports: [
     MonedaModule,
     CuentaHistorialModule,
+    UtilsModule,
     forwardRef(() => UserModule),
     MongooseModule.forFeature([
       { name: Cuenta.name, schema: CuentaSchema },
       { name: User.name, schema: UserSchema },
+      { name: Subcuenta.name, schema: SubcuentaSchema },
     ]),
   ],
   controllers: [CuentaController],
