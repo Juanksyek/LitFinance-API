bb6cc585e76b48f6be0fb4ec58e017c1b9f9d8f2
Juan Flores <elgalleto12393@gmail.com>
2025-11-27 22:44:27 -0600

    feat: enhance UtilsModule to include ConversionService and Moneda schema integration

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MoneyValidationService } from './validators/money-validation.service';
+import { ConversionService } from './services/conversion.service';
+import { Moneda, MonedaSchema } from '../moneda/schema/moneda.schema';
 
 @Module({
-  providers: [MoneyValidationService],
-  exports: [MoneyValidationService],
+  imports: [
+    MongooseModule.forFeature([{ name: Moneda.name, schema: MonedaSchema }]),
+  ],
+  providers: [MoneyValidationService, ConversionService],
+  exports: [MoneyValidationService, ConversionService],
 })
 export class UtilsModule {}
