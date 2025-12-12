e2c75358a6e209b783d80e5567281e033cc2455a
Juan Flores <elgalleto12393@gmail.com>
2025-12-12 00:22:56 -0600

    feat: update NotificacionesModule to include User schema and NotificacionesCronService

--- src/notificaciones/notificaciones.module.ts
+++ src/notificaciones/notificaciones.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotificacionesService } from './notificaciones.service';
 import { NotificacionesController } from './notificaciones.controller';
+import { NotificacionesCronService } from './notificaciones-cron.service';
 import { DispositivoUsuario, DispositivoUsuarioSchema } from './schemas/dispositivo-usuario.schema';
+import { User, UserSchema } from '../user/schemas/user.schema/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: DispositivoUsuario.name, schema: DispositivoUsuarioSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
-  providers: [NotificacionesService],
+  providers: [NotificacionesService, NotificacionesCronService],
   controllers: [NotificacionesController],
   exports: [NotificacionesService],
 })
