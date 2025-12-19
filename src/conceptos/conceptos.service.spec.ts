15e2e5faeedaa8354aec28fe6f0220f1c7528cc7
Juan Flores <elgalleto12393@gmail.com>
2025-12-19 17:42:59 -0600

    feat: add mock provider for ConceptoPersonalizado in ConceptosService tests

--- src/conceptos/conceptos.service.spec.ts
+++ src/conceptos/conceptos.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConceptosService } from './conceptos.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('ConceptosService', () => {
   let service: ConceptosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConceptosService],
+      providers: [
+        ConceptosService,
+        {
+          provide: getModelToken('ConceptoPersonalizado'),
+          useValue: {
+            find: jest.fn(),
+            findById: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<ConceptosService>(ConceptosService);
