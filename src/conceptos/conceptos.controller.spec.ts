10f5885ab59528371360932a28e8b83ea3658e7a
Juan Flores <elgalleto12393@gmail.com>
2025-12-19 17:42:56 -0600

    feat: add mockConceptosService to unit tests for ConceptosController

--- src/conceptos/conceptos.controller.spec.ts
+++ src/conceptos/conceptos.controller.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConceptosController } from './conceptos.controller';
+import { ConceptosService } from './conceptos.service';
 
 describe('ConceptosController', () => {
   let controller: ConceptosController;
 
+  const mockConceptosService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ConceptosController],
+      providers: [
+        { provide: ConceptosService, useValue: mockConceptosService },
+      ],
     }).compile();
 
     controller = module.get<ConceptosController>(ConceptosController);
