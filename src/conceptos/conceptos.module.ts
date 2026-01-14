0c15ae44d2c43d9115e2e5bf088412e173e2c67a
Juan Flores <elgalleto12393@gmail.com>
2026-01-13 22:12:15 -0600

    feat: add CuentaHistorial and Transaction schemas to ConceptosModule

--- src/conceptos/conceptos.module.ts
+++ src/conceptos/conceptos.module.ts
@@ -3,11 +3,15 @@ import { ConceptosService } from './conceptos.service';
 import { ConceptosController } from './conceptos.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConceptoPersonalizado, ConceptoPersonalizadoSchema } from './schemas/concepto-personalizado.schema';
+import { CuentaHistorial, CuentaHistorialSchema } from '../cuenta-historial/schemas/cuenta-historial.schema';
+import { Transaction, TransactionSchema } from '../transactions/schemas/transaction.schema/transaction.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: ConceptoPersonalizado.name, schema: ConceptoPersonalizadoSchema },
+      { name: CuentaHistorial.name, schema: CuentaHistorialSchema },
+      { name: Transaction.name, schema: TransactionSchema },
     ]),
   ],
   controllers: [ConceptosController],
