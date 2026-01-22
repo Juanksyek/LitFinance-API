dfed6f0403204c67482f3f852e4dbab6bc528a5c
Juan Flores <elgalleto12393@gmail.com>
2026-01-22 16:14:59 -0600

    feat: add PasswordReset schema and service to AuthModule with corresponding controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
@@ -8,13 +9,18 @@ import { EmailModule } from '../email/email.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { CuentaModule } from '../cuenta/cuenta.module';
 import { MonedaModule } from '../moneda/moneda.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserSession, UserSessionSchema } from './schemas/user-session.schema';
+import { PasswordReset, PasswordResetSchema } from './schemas/password-reset.schema';
+import { PasswordResetService } from './password-reset.service';
+import { PasswordResetController } from './password-reset.controller';
 
 @Module({
   imports: [
     UserModule,
-    MongooseModule.forFeature([{ name: UserSession.name, schema: UserSessionSchema }]),
+    MongooseModule.forFeature([
+      { name: UserSession.name, schema: UserSessionSchema },
+      { name: PasswordReset.name, schema: PasswordResetSchema },
+    ]),
     EmailModule,
     CuentaModule,
     MonedaModule,
@@ -26,7 +32,7 @@ import { UserSession, UserSessionSchema } from './schemas/user-session.schema';
       }),
     }),
   ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController, PasswordResetController],
+  providers: [AuthService, JwtStrategy, PasswordResetService],
 })
 export class AuthModule {}
