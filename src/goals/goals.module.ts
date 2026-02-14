f30dd828b23dd1df6ebb485e91732da59e5b1321
Juan Flores <elgalleto12393@gmail.com>
2026-02-14 00:33:41 -0600

    feat: enhance GoalsModule with InternalTransferService and additional schemas

--- src/goals/goals.module.ts
+++ src/goals/goals.module.ts
@@ -1,9 +1,45 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { GoalsController } from './goals.controller';
 import { GoalsService } from './goals.service';
 
+import { Meta, MetaSchema } from './schemas/meta.schema';
+import { MetaEvento, MetaEventoSchema } from './schemas/meta-evento.schema';
+import { InternalTransfer, InternalTransferSchema } from './schemas/internal-transfer.schema';
+import { InternalTransferService } from './services/internal-transfer.service';
+
+import { SubcuentaModule } from '../subcuenta/subcuenta.module';
+import { CuentaModule } from '../cuenta/cuenta.module';
+import { CuentaHistorialModule } from '../cuenta-historial/cuenta-historial.module';
+import { UserModule } from '../user/user.module';
+import { UtilsModule } from '../utils/utils.module';
+import { PlanConfigModule } from '../plan-config/plan-config.module';
+
+import { Cuenta, CuentaSchema } from '../cuenta/schemas/cuenta.schema/cuenta.schema';
+import { Subcuenta, SubcuentaSchema } from '../subcuenta/schemas/subcuenta.schema/subcuenta.schema';
+import { CuentaHistorial, CuentaHistorialSchema } from '../cuenta-historial/schemas/cuenta-historial.schema';
+import { SubcuentaHistorial, SubcuentaHistorialSchema } from '../subcuenta/schemas/subcuenta-historial.schema/subcuenta-historial.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Meta.name, schema: MetaSchema },
+      { name: MetaEvento.name, schema: MetaEventoSchema },
+      { name: InternalTransfer.name, schema: InternalTransferSchema },
+      { name: Cuenta.name, schema: CuentaSchema },
+      { name: Subcuenta.name, schema: SubcuentaSchema },
+      { name: CuentaHistorial.name, schema: CuentaHistorialSchema },
+      { name: SubcuentaHistorial.name, schema: SubcuentaHistorialSchema },
+    ]),
+    CuentaModule,
+    SubcuentaModule,
+    CuentaHistorialModule,
+    UserModule,
+    UtilsModule,
+    PlanConfigModule,
+  ],
   controllers: [GoalsController],
-  providers: [GoalsService],
+  providers: [GoalsService, InternalTransferService],
 })
 export class GoalsModule {}
